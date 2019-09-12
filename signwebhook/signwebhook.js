6efe4089bf5f3af84a8405000e153334df00db27
filmaj <maj.fil@gmail.com>
2019-09-12 15:14:11 -0400

    the sign webhook should be up and functional!

--- signwebhook/signwebhook.js
+++ signwebhook/signwebhook.js
@@ -15,13 +15,23 @@ const request = require('request-promise-native');
 */
 const utils = require('../utils.js');
 const config = utils.get_config();
+const openwhisk = require('openwhisk');
+const github_app = require('github-app');
+
+const INSTALLATION_IDS = {
+  adobe: 531387,
+  AdobeDocs: 574581,
+  hubblestack: 840208,
+  magento: 1375071
+};
+const OUR_ORGS = Object.keys(INSTALLATION_IDS);
 
 async function main (params) {
   let headers = {
     'Content-Type': 'application/json'
   };
   if (params && params['__ow_headers'] && (params['__ow_headers']['X-AdobeSign-ClientId'] || params['__ow_headers']['X-ADOBESIGN-CLIENTID'] || params['__ow_headers']['x-adobesign-clientid'])) {
-    // Verification of intent from Adobe Sign: https://helpx.adobe.com/sign/using/adobe-sign-webhooks-api.html#VoI
+    // This `if` block is the "Verification of intent" from Adobe Sign: https://helpx.adobe.com/sign/using/adobe-sign-webhooks-api.html#VoI
     let client_id = params['__ow_headers']['X-AdobeSign-ClientId'] || params['__ow_headers']['X-ADOBESIGN-CLIENTID'] || params['__ow_headers']['x-adobesign-clientid'];
     if (client_id === config.signClientID) {
       // We are responding to a request from a webhook created by us;
@@ -41,24 +51,110 @@ async function main (params) {
       };
     }
   }
-  // Just echo out what we received from the webhook payload for now
-  return {
-    statusCode: 200,
-    headers,
-    body: params
-  };
-  /*
-  // Get an access_token from a refresh_token for Adobe Sign APIs
-  let response;
-  try {
-    response = await utils.get_adobe_sign_access_token(config);
-  } catch (e) {
-    return utils.action_error(e, 'Error during retrieval of Adobe Sign access token.');
+  if (params.actionType !== 'ESIGNED' || !params.agreement || !params.agreement.id || !params.agreement.name || params.agreement.name !== 'Adobe CLA') {
+    // If the needed parameters are not a part of this invocation, ignore.
+    return {
+      statusCode: 400,
+      headers,
+      body: 'Insufficient parameters for processing, aborting.'
+    };
   }
-  const access_token = response.access_token;
-  if (!access_token) {
-    return { statusCode: 500, body: 'Empty access_token retrieved from Adobe Sign.' };
-  */
+  const ow = openwhisk();
+  let lookup_res;
+  try {
+    lookup_res = await ow.actions.invoke({
+      name: utils.LOOKUP,
+      blocking: true,
+      result: true,
+      params: {
+        agreements: params.agreement.id,
+        apiVersion: 'v6'
+      }
+    });
+  } catch (e) {
+    return {
+      statusCode: 500,
+      headers,
+      body: `Error invoking lookup action when agreements were found: ${e}`
+    };
+  }
+  let usernames = lookup_res.body.usernames;
+  let user_query = usernames.map(u => `author:${u}`).join(' ');
+  const app = github_app({
+    id: config.githubAppId,
+    cert: config.githubKey
+  });
+  let errors = [];
+  let completed = [];
+  // for each of our orgs:
+  await Promise.all(OUR_ORGS.map(async (org) => {
+    // 1. get a github client for each org
+    let installation_id = INSTALLATION_IDS[org];
+    let github;
+    try {
+      github = await app.asInstallation(installation_id);
+    } catch (e) {
+      errors.push(`Error retrieving GitHub API instance on behalf of app installation for ${org}: ${e}`);
+      return;
+    }
+    // 2. use each installation's client to use the search api
+    // https://developer.github.com/v3/search/#search-issues-and-pull-requests
+    // to look for open pull requests from the signed user(s)
+    // e.g. is:pr is:open org:adobe author:filmaj
+    let search_results;
+    try {
+      search_results = await github.search.issues({ q: `is:pr is:open org:${org} ${user_query}` });
+    } catch (e) {
+      errors.push(`Error retrieving GitHub API instance on behalf of app installation for ${org}: ${e}`);
+      return;
+    }
+    if (search_results.data.total_count) {
+      // If there are some PRs open, then we send the org, repo, pr number and
+      // head SHA of the PR over to the setgithubcheck action, which will render
+      // the green checkmark on the PR!
+      await Promise.all(search_results.data.items.map(async (pr) => {
+        let repo_url = pr.repository_url.split('/');
+        let repo = repo_url[repo_url.length - 1];
+        let pr_data;
+        try {
+          pr_data = await github.pullRequests.get({
+            owner: org,
+            repo,
+            number: pr.number
+          });
+        } catch (e) {
+          errors.push(`Error retrieving pull request for ${org}/${repo}#${pr.number}: ${e}`);
+          return;
+        }
+        let sha = pr_data.data.head.sha;
+        try {
+          await ow.actions.invoke({
+            name: utils.SETGITHUBCHECK,
+            blocking: true,
+            result: true,
+            params: {
+              installation_id: installation_id,
+              org: org,
+              repo: repo,
+              sha,
+              status: 'completed',
+              conclusion: 'success',
+              title: 'CLA Signed',
+              summary: 'A Signed CLA has been found for the GitHub.com user ' + pr_data.data.user.login
+            }
+          });
+        } catch (e) {
+          errors.push(`Error invoking setgithubcheck for ${org}/${repo}#${pr.number}: ${e}`);
+        }
+        completed.push(`${org}/${repo}#${pr.number}`);
+      }));
+    }
+  }));
+  return {
+    statusCode: 201,
+    headers,
+    body: errors.length ? errors.join('\n') : `Completed without failure: ${completed.join('\n')}`
+  };
 }
 
 exports.main = main;
